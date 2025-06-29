09186ed7609ed73dfbf74f126046614f8af42e2f
joaofuzeto <fuzeto.jf@gmail.com>
2025-06-29 19:54:18 +0100

    Primeiro commit com o readme definindo os objetivos de cada exercício proposto

---
+++ Challenges/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira um número ao qual queira multiplicar: ");
+        int number = scanner.nextInt();
+
+        if(number != 0){
+
+        }
+    }
+}
